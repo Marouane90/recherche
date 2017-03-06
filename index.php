8af70faa949e5a8bc20098c584035f767108ef1e
hansmanng <hansmann.g@gmail.com>
2017-03-06 16:41:14 +0100

    apps index2

--- index.php
+++ index.php
@@ -1,19 +1,18 @@
 <?php
 $errors = [];
 $page = "categories";
-$db = mysqli_connect("192.168.1.57","animaniax","animaniax","animaniax");???????????
+$db = mysqli_connect("192.168.1.57","animaniax","animaniax","animaniax");
 session_start();
 $access = ["search", "search_elem"];
-if (isset($_GET['page']) && in_array($_GET['page'], $access))
-{
-    $page = $_GET['page'];
-}
+// if (isset($_GET['page']) && in_array($_GET['page'], $access))
+// {
+    // $page = $_GET['page'];
+// }
 function __autoload($classname)// http://php.net/manual/fr/function.autoload.php
 {
 	require('models/'.$classname.'.class.php');
 }
-$access_traitement = ["logout"=>"users", "login"=>"users", "register"=>"users", "create_category"=>"category", "cart"=>"orders",
-						"user"=>"users", "create_product"=>"Products", "product"=>"orders"]; // comments
+$access_traitement = ["search"=>"books", "search_elem"=>"books"]; // comments
 if (isset($_GET['page'], $access_traitement[$_GET['page']]))
 {
 	$traitement = $access_traitement[$_GET['page']];

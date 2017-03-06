ea7915bfacce6a3e3401c12af2d9fb28c743a32a
hansmanng <hansmann.g@gmail.com>
2017-03-06 16:54:37 +0100

    apps models04

--- models/bookManager.class.php
+++ models/bookManager.class.php
@@ -1,5 +1,5 @@
 <?php
-class ProductsManager
+class BookManager
 {
 	private $db;
 
@@ -12,9 +12,28 @@ class ProductsManager
 		$list = [];
 		$recherche = mysqli_real_escape_string($this->db, $search);
 		$res = mysqli_query($this->db, "SELECT * FROM books WHERE name LIKE '%".$recherche."%' OR author LIKE '%".$recherche."%' OR country LIKE '%".$recherche."%' OR gender LIKE '%".$recherche."%'OR year LIKE '%".$recherche."%' OR editorial LIKE '%".$recherche."%'");
-		while($product = mysqli_fetch_object($res, "Products", [$this->db]))
+		while($product = mysqli_fetch_object($res, "books", [$this->db]))
 		{
 			$list[] = $books;
 		}
 		return $list;
+	}
+	public function findById($id)
+	{
+		$id=intval($id); 
+		$res = mysqli_query($this->db, "SELECT * FROM books WHERE id='".$id."'");
+		$user = mysqli_fetch_object($res, "books", [$this->db]); 
+		return $user;
+	}
+	public function findByAuthor($search)
+	{
+		$list = [];
+		$author = mysqli_real_escape_string($this->db, $search);
+		$res = mysqli_query($this->db, "SELECT * FROM books WHERE author='".$author."' ORDER BY author DESC");
+		while ($author = mysqli_fetch_object($res, "books", [$this->db]))
+		{
+			$list[] = $author;
+		}
+		return $list;
+		var_dump($author);
 	}

95cb5088aec4d780b605d5b018b7d4a71105b4db
hansmanng <hansmann.g@gmail.com>
2017-03-07 15:12:26 +0100

    models update 04

--- models/Book.class.php
+++ models/Book.class.php
@@ -1,5 +1,5 @@
 <?php
-class books
+class Book
 {
 	private $id;
 	private $name; 
--- models/BookManager.class.php
+++ models/BookManager.class.php
@@ -16,43 +16,65 @@ class BookManager
 	// 	return $books;
 	// }
 
-	public function search($name, $author, $country, $gender, $year, $editorial)
+	public function search($isbn, $price_min, $price_max, $year_min, $year_max, $gender, $name, $author, $country, $editorial)
 	{
-		$request = "SELECT * FROM books WHERE";
-		if ($name != "")
+		$request = "SELECT * FROM books WHERE 1 ";
+
+		if ($isbn!= "")
 		{
-			$name = mysqli_real_escape_string($this->db, $name);
-			$request .= "name LIKE '%".$name."%' ";
+			$isbn = mysqli_real_escape_string($isbn);
+			$request .= " AND isbn LIKE '%".$isbn."%' ";
 	    }
-	    if ($author != "")
+	    if ($price_min!= "")
 		{
-			$author = mysqli_real_escape_string($this->db, $author);
-			$request .= "author LIKE '%".$author."%' ";
+			$price_min = intval($price_min);
+			$request .= "AND price <= '".$price_min."' ";
 	    }
-	    if ($country != "")
+	    if ($price_max!= "")
 		{
-			$country = mysqli_real_escape_string($this->db, $country);
-			$request .= "country LIKE '%".$country."%' ";
+			$price_max = intval($price_max);
+			$request .= "AND price >= '".$price_max."' ";
+	    }
+	    if ($year_min!= "")
+		{
+			$year_min = intval($year_min);
+			$request .= "AND YEAR(year) >='".$year_min."' ";
+	    }
+	    if ($year_max!= "")
+		{
+			$year_max = intval($year_max);
+			$request .= "AND YEAR(year) <= '".$year_max."' ";
 	    }
 	    if ($gender != "")
 		{
 			$gender = mysqli_real_escape_string($this->db, $gender);
-			$request .= "gender LIKE '%".$gender."%' ";
+			$request .= "AND gender LIKE '%".$gender."%' ";
 	    }
-	    if ($year!= "")
+		if ($name != "")
 		{
-			$year = intval($year);
-			$request .= "year LIKE '%".$year."%' ";
+			$name = mysqli_real_escape_string($this->db, $name);
+			$request .= "AND name LIKE '%".$name."%' ";
+	    }
+	    if ($author != "")
+		{
+			$author = mysqli_real_escape_string($this->db, $author);
+			$request .= "AND author LIKE '%".$author."%' ";
+	    }
+	    if ($country != "")
+		{
+			$country = mysqli_real_escape_string($this->db, $country);
+			$request .= "AND country LIKE '%".$country."%' ";
 	    }
 	    if ($editorial != "")
 		{
 			$editorial = mysqli_real_escape_string($this->db, $editorial);
-			$request .= "editorial LIKE '%".$editorial."%' ";
+			$request .= "AND editorial LIKE '%".$editorial."%' ";
 	    }
 
         $request .= "ORDER BY name DESC";
         $list=[];
 		$res = mysqli_query($this->db, $request);
+		var_dump(mysqli_error($this->db));
 		while ($books = mysqli_fetch_object($res, "Book", [$this->db]))
 		{
 			$list[] = $books;
@@ -69,6 +91,17 @@ class BookManager
 			$list[] = $gender['gender'];
 		}
 		return $list;
+	}
+	public function findAll()
+	{
+		$list = [];
+		$res = mysqli_query($this->db, "SELECT * FROM books ORDER BY gender DESC LIMIT 10");
+		while ($books = mysqli_fetch_object($res, "Book", [$this->db]))
+		{
+			$list[] = $books;
+		}
+		return $list;
+
 	}
 }
 	

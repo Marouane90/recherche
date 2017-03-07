6b9377395590a82acf54d62e45d28d41b8e1e935
hansmanng <hansmann.g@gmail.com>
2017-03-07 11:57:30 +0100

    search

--- apps/search.php
+++ apps/search.php
@@ -1,12 +1,4 @@
 <?php
-<<<<<<< HEAD
-// if (isset($_GET['page'], $_GET['author'])
-// {
-	require('views/search.phtml');
-// }
-=======
-$manager = new Manager($db);
-$genders = $manager->findGenders();
+
 require('views/search.phtml');
->>>>>>> a7398715feae2fb0b2ba0b9b2e9ced2bf3aef013
-?>
+

6e2bef7bb4e4fa637d6c183d08b4abe7068bf03c
hansmanng <hansmann.g@gmail.com>
2017-03-08 11:29:33 +0100

    update script

--- public/script/script.js
+++ public/script/script.js
@@ -1,10 +1,9 @@
 $(document).ready(function()
 {
-	$('.form-control').keyup(function(){
-
-	}
+	$('.form-control').keyup(function()
+	{  
 		  var isbn = encodeURIComponent($('#isbn').val());
-		  var price_min = encodeURIComponent($('#prince_min').val());
+		  var price_min = encodeURIComponent($('#price_min').val());
 		  var price_max = encodeURIComponent($('#price_max').val());
 		  var year_min = encodeURIComponent($('#year_min').val());
 		  var year_max = encodeURIComponent($('#year_max').val());
@@ -14,5 +13,6 @@ $(document).ready(function()
 		  var country = encodeURIComponent($('#country').val());
 		  var editorial = encodeURIComponent($('#editorial').val());
 
-  $('#resultats').load("index.php?page=search_elem&ajax&isbn="+isbn+"&price_min="+price_min+"&price_max="+price_max+"&year_min="+year_min+"&year_max="+year_max+"&gender="+gender+"&name="+name+"&author="+author+"&country="+country+"&editorial="+editorial);
+		  $('#resultats').load("index.php?page=search_elem&ajax&isbn="+isbn+"&price_min="+price_min+"&price_max="+price_max+"&year_min="+year_min+"&year_max="+year_max+"&gender="+gender+"&name="+name+"&author="+author+"&country="+country+"&editorial="+editorial);
+  	});
 });
